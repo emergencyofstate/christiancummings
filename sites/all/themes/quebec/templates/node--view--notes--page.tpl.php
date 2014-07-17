245617b6f4cea0fc6375d43ce6f3ba09d32b48b3
emergencyofstate <emergencyofstate@gmail.com>
2014-07-16 23:17:27 -0700

    Removing comments from the view display

--- sites/all/themes/quebec/templates/node--view--notes--page.tpl.php
+++ sites/all/themes/quebec/templates/node--view--notes--page.tpl.php
@@ -15,7 +15,7 @@
 				<p class="post_meta">
 				  <i class="greytext icon-user"></i><span class="smallfont greytext"> by <?php print $name; ?></span> &nbsp; &nbsp;
 					<i class="greytext icon-time"></i><span class="smallfont greytext"> <?php print format_date($node->created, 'custom', 'M d, Y'); ?></span> &nbsp; &nbsp;
-					<i class="greytext icon-comment"></i> <a href="<?php print $node_url;?>/#comments"><?php print $comment_count; ?> comments</a>&nbsp; &nbsp; 
+
 					<?php if ($field_tags):?>
 					<i class="greytext icon-folder-open"></i><?php print render($content['field_tags']); ?>
 				  <?php endif; ?>
